aa5ad98f35e7b01c154e8230e31c87c989cc6b89
Briana Castillo <bricastillo96@gmail.com>
2017-11-19 18:06:30 +0000

    install postgres and install database

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -5,12 +5,32 @@ class TodoController < ApplicationController
         todo_id= params[:id]
         
         if todo_id=='1'
-            @todo_description = ''
+            @todo_description = 'Make the Curriculum'
             @todo_pomodoro = 4
             
-        elsif 
-            @todo_id== '2'
-            @todo_description=''
+        elsif todo_id=='2' 
+            @todo_pomodoro= 3
+            @todo_description='Bring Workshop Supplies'
+            
+        elsif todo_id=='3' 
+            @todo_pomodoro= 2
+            @todo_description='Meet with volunteer trainers'
+            
+        elsif todo_id=='4' 
+            @todo_pomodoro= 3
+            @todo_description='Order food for Saturday and Sunday'
+            
+        elsif todo_id=='5' 
+            @todo_pomodoro= 1
+            @todo_description='Check Stuff'
+            
+        elsif todo_id=='6' 
+            @todo_pomodoro= 1
+            @todo_description='Send workshop location to students'
+            
+        elsif todo_id=='7' 
+            @todo_pomodoro= 0
+            @todo_description='Have a great workshop'
         end
     end
 end
